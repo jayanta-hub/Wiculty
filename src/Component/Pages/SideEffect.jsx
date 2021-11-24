c65ce143e6255979541ba8c5d1fbfacbc8b50d59
jayanta-hub <jayanta.garu@gmail.com>
2021-11-24 20:26:27 +0530

    commit

--- src/Component/Pages/SideEffect.jsx
+++ src/Component/Pages/SideEffect.jsx
@@ -12,19 +12,16 @@ const SideEffect = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  // useEffect(() => {
-  //   setCalculation(() => count * 2);
-  // }, [count]);
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]);
 
-  //   useEffect(() => {
-  //     let timer = setTimeout(() => {
-  //     setCount((count) => count + 1);
-  //   }, 1000);
-
- 
-
-  //   return () => clearTimeout(timer)
-  //   }, []);
+    // useEffect(() => {
+    //   let timer = setTimeout(() => {
+    //   setCount((count) => count + 1);
+    // }, 1000);
+    // return () => clearTimeout(timer)
+    // }, []);
   return (
     <>
       <h1>hello</h1>
@@ -33,42 +30,9 @@ const SideEffect = () => {
       <h1>hello</h1>
       <h1>{count}</h1>
       <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <button className="btn bg-primary" onClick={() => setCount( count + 1)}>+</button>
       <p>Calculation: {calculation}</p>
 
-      {show1 ? (
-
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      ) : (
-        ""
-      )}
-
-     
-
-        {/* <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card> */}
      
     </>
   );
